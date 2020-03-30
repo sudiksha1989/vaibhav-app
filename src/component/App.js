db11e308ce6a34b2594e6b4d7585e61ab0e984b7
sudiksha1989 <snagvanshi06@gmail.com>
2020-03-30 11:53:47 +0530

    updated disign

--- src/component/App.js
+++ src/component/App.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import { IMAGES } from './constant'
 
-import {Container, CardColumns, Card, Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import { Container, CardColumns, Card, Navbar, Nav, NavDropdown } from 'react-bootstrap';
 import WOW from "wow.js";
 
 class App extends React.Component {
@@ -14,29 +14,43 @@ class App extends React.Component {
     return (
 
       <>
-        <Navbar collapseOnSelect fixed="top" expand="lg" bg="black" variant="dark">
-          <Navbar.Brand href="#home">
-            <img
-              src={IMAGES.logo}
-              width="50"
-              height="40"
-              className="d-inline-block align-top"
-              alt="React Bootstrap logo"
-            />PerspectivesbyV</Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="mr-auto">              
-            </Nav>
-            <Nav>
-            <Nav.Link href="#features">Portfolio</Nav.Link>
-              <Nav.Link href="#pricing">Landscape</Nav.Link>
-              <Nav.Link href="#pricing">Prints</Nav.Link>
-              <Nav.Link href="#deets">Contact/About</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
-
-
+        <div class='about-banner-sec'>
+          <Navbar collapseOnSelect fixed="top" expand="lg" bg="black" variant="dark">
+            <Navbar.Brand href="#home">
+              <img
+                src={IMAGES.logo}
+                width="50"
+                height="40"
+                className="d-inline-block align-top"
+                alt="React Bootstrap logo"
+              />PerspectivesbyV</Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="mr-auto">
+              </Nav>
+              <Nav>
+                <Nav.Link href="#features">Portfolio</Nav.Link>
+                <Nav.Link href="#pricing">Landscape</Nav.Link>
+                <Nav.Link href="#pricing">Prints</Nav.Link>
+                <Nav.Link href="#deets">Contact/About</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+          <div class="about-banner-text wow fadeInDown" data-wow-duration="2s"
+            style={{
+              visibility: 'visible',
+              animationDuration: '2s',
+              animationName: 'fadeInDown',
+            }}>
+            <div class="containerDiv">
+              <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                <h1>Welcome</h1>
+                <div class="about-banner-text-divide"></div>
+                <p>We integrate tech expertise and business intelligence which combines with our role as one of the most trusted consultants for Indian business clients.</p>
+              </div>
+            </div>
+          </div>
+        </div>
         <Container>
           <CardColumns>
             <div className='col_half'>
